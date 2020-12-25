d97f719614cd03bf81cc7a4739d4bdfecf45db52
SimonWang00 <simon_wang00@163.com>
2020-12-25 17:01:19 +0800

    delete unuse

--- docker/build.sh
+++
@@ -1,7 +0,0 @@
-CGO_ENABLED=0 GOOS=linux go build -a -ldflags '-extldflags "-static"' -o agent .
-
-sudo docker-compose -f docker-compose.yml up
-
-sudo docker-compose -f docker-compose.yml up agent
-
-sudo docker-compose -f docker-compose.yml down
