a1fc74b29afdab27ffbf113378d41ea827275642
SimonWang00 <simon_wang00@163.com>
2021-01-05 17:50:31 +0800

    代理服务器

--- 25.工具库-为http添加代理服务器/proxyservice/main.go
+++ 25.工具库-为http添加代理服务器/proxyservice/main.go
@@ -5,6 +5,26 @@ package main
 //Describe: describle your function
 //Date  : 2021/1/5
 
+import (
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
 func main() {
-	$END$
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		parse, err := url.Parse("http://127.0.0.1:8081")
+		if err != nil{
+			panic(err)
+		}
+		log.Println("proxy server accept request!")
+		proxy := httputil.NewSingleHostReverseProxy(parse)
+		proxy.ServeHTTP(writer, request)
+	})
+
+	log.Println("proxy server success :", 8080)
+	_ = http.ListenAndServe("127.0.0.1:8080", mux)
 }
--- 25.工具库-为http添加代理服务器/service/main.go
+++ 25.工具库-为http添加代理服务器/service/main.go
@@ -1,10 +1,21 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 //File  : main.go
 //Author: Simon
 //Describe: describle your function
 //Date  : 2021/1/5
 
 func main() {
-	$END$
+	mux := http.NewServeMux()
+	mux.HandleFunc("/do", func(writer http.ResponseWriter, request *http.Request) {
+		log.Println("service is running")
+		writer.Write([]byte("service done!"))
+	})
+	log.Println("server sucess:", 8080)
+	_ = http.ListenAndServe("127.0.0.1:8081", mux)
 }
