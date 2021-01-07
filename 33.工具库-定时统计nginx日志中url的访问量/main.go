0e9958b369047417b3d66d8ce2c2502897d7f61f
SimonWang00 <simon_wang00@163.com>
2021-01-07 18:45:17 +0800

    定时统计nginx日志中url的访问量

--- 33.工具库-定时统计nginx日志中url的访问量/main.go
+++ 33.工具库-定时统计nginx日志中url的访问量/main.go
@@ -2,9 +2,106 @@ package main
 
 //File  : main.go
 //Author: Simon
-//Describe: describle your function
+//Describe: 统计nginx 访问量
 //Date  : 2021/1/7
 
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"github.com/hpcloud/tail"
+	"github.com/robfig/cron"
+	"os"
+	"sort"
+	"strings"
+	"sync"
+	"time"
+)
+
+var (
+	listArray []numMap
+	mu sync.Mutex
+	statisticsUrl map[string]int
+	urlIndex = flag.Int("i", 5, "url index")
+	cronTime = flag.String("t", "*/5 * * * * ?", "cron Time")
+	filePath = flag.String("p", "./nginx.log", "nginx log path")
+)
+
+// 统计链接的map
+type numMap struct {
+	key string
+	num int
+}
+
 func main() {
-	$END$
+	flag.Parse()
+	config := tail.Config{
+		ReOpen:    true,                                 // 重新打开
+		Follow:    true,                                 // 是否跟随
+		Location:  &tail.SeekInfo{Offset: 0, Whence: 2}, // 从文件的哪个地方开始读
+		MustExist: false,                                // 文件不存在不报错
+		Poll:      true,
+	}
+	statisticsUrl = make(map[string]int)
+	t, err := tail.TailFile(*filePath, config)
+	if err != nil {
+		fmt.Println("TailFile", err)
+		return
+	}
+	// 定时任务
+	go func() {
+		c := cron.New()
+		if err := c.AddFunc(*cronTime, func() {
+			f, err := os.Create(fmt.Sprintf("./nginx_statistics_%v.log", time.Now().Format("2006-01-02_15:04:05")))
+			if err != nil {
+				fmt.Println("Create", err)
+				return
+			}
+			defer f.Close()
+			w := bufio.NewWriter(f)
+			mu.Lock()
+			for k, v := range statisticsUrl {
+				listArray = append(listArray, numMap{
+					key: k,
+					num: v,
+				})
+			}
+			sort.Slice(listArray, func(i, j int) bool {
+				return listArray[i].num > listArray[j].num
+			})
+			for _, v := range listArray {
+				_, _ = fmt.Fprintln(w, fmt.Sprintf("%d     %s", v.num, v.key))
+			}
+			_ = w.Flush()
+			statisticsUrl = make(map[string]int)
+			listArray = []numMap{}
+			mu.Unlock()
+		}); err != nil {
+			fmt.Println("AddFunc", err)
+			return
+		}
+		c.Start()
+	}()
+
+
+	for {
+		select {
+		case info := <-t.Lines:
+			i := strings.Split(info.Text, " ")
+			for index, v := range i {
+				if index == *urlIndex {
+					// 统计url访问量，加锁， map多线程不安全
+					mu.Lock()
+					val, ok := statisticsUrl[v]
+					if ok {
+						val = val + 1
+						statisticsUrl[v] = val
+					} else {
+						statisticsUrl[v] = 1
+					}
+					mu.Unlock()
+				}
+			}
+		}
+	}
 }
