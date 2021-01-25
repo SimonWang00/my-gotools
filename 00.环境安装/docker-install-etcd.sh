e8df55c8b910a8ccd0ed284daa825dc05f9781d6
王小猛 <bw_wangxiaomeng@whty.com.cn>
2021-01-25 14:20:10 +0800

    更新etcd启动方式

--- 00.环境安装/docker-install-etcd.sh
+++ 00.环境安装/docker-install-etcd.sh
@@ -8,7 +8,7 @@ docker pull quay.io/coreos/etcd:latest
 docker images
 
 # 4. 运行容器
-docker run -itd --name etcd-test -p 2379:2379 quay.io/coreos/etcd
+docker run -d -p 2379:2379 -p 2380:2380 --name my-etcd quay.io/coreos/etcd /usr/local/bin/etcd --name s1 --data-dir /etcd-data --listen-client-urls http://0.0.0.0:2379 --advertise-client-urls http://0.0.0.0:2379 --listen-peer-urls http://0.0.0.0:2380 --initial-advertise-peer-urls http://0.0.0.0:2380 --initial-cluster s1=http://0.0.0.0:2380 --initial-cluster-token tkn --initial-cluster-state new
 
 # 5. 安装成功
 docker exec -it etcd-test /bin/bash
